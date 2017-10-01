f2273d6382ad8d47f568d1567702d41342c66649
therohin <rohintak@gmail.com>
2017-10-01 20:10:01 +0530

    Chapter 11 16 pages done,
    Chapter 11 16 pages done, Also added Console.WriteLine in the MainActivity.cs

--- PhoneCallApp/MainActivity.cs
+++ PhoneCallApp/MainActivity.cs
@@ -2,6 +2,7 @@
 using Android.Widget;
 using Android.OS;
 using Android.Content;
+using System;
 
 namespace PhoneCallApp
 {
@@ -19,16 +20,21 @@ namespace PhoneCallApp
 
             phoneNumberInput = FindViewById<EditText>(Resource.Id.PhoneNumber);
             callButton = FindViewById<Button>(Resource.Id.CallButton);
-                
+
+            Console.WriteLine("Input Field Id and Call Button found");
 
             callButton.Click += CallButton_Click;
         }
         
         private void CallButton_Click(object sender, System.EventArgs e)
         {
+            Console.WriteLine("Call Button Clicked");
+
             var phoneNumber = phoneNumberInput.Text;
             if(!string.IsNullOrWhiteSpace(phoneNumber))
             {
+                Console.WriteLine("Phone Number entered: " + phoneNumber);
+
                 var callDialog = new AlertDialog.Builder(this);
                 callDialog.SetMessage("Do you want to call " + phoneNumber + "?");
                 //Set Call and Cancel Button for the call dialog
@@ -36,6 +42,9 @@ namespace PhoneCallApp
                     // Create intent to dial phone
                     var callIntent = new Intent(Intent.ActionCall);
                     callIntent.SetData(Android.Net.Uri.Parse("tel:" + phoneNumber));
+
+                    Console.WriteLine("Calling on Phone Number: " + phoneNumber);
+
                     StartActivity(callIntent);
                 });
                 callDialog.SetNegativeButton("Cancel", delegate { });
@@ -45,6 +54,8 @@ namespace PhoneCallApp
             }
             else
             {
+                Console.WriteLine("Phone number not entered by user");
+
                 var toast = Toast.MakeText(this, "Please provide number", new ToastLength());
                 toast.Show();
             }
